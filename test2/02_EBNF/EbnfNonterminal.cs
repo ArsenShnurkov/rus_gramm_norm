6750cb0256a5afcd3a896260f71874b81870adfa
ArsenShnurkov <ArsenShnurkov@users.noreply.github.com>
2019-01-05 03:18:23 +0300

    IEbnfRulePart

---
+++ test2/02_EBNF/EbnfNonterminal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace test2
+{
+    public class EbnfNonterminal : NonterminalSymbol
+    {
+        Dictionary<EbnfRule, EbnfRule> rules = new Dictionary<EbnfRule, EbnfRule> ();
+        public EbnfNonterminal (string name) : base (name)
+        {
+        }
+        public IEnumerable<EbnfRule> Rules { get { return rules.Values; } }
+        public void Add (EbnfRule rule)
+        {
+            if (rules.ContainsKey (rule) == false) {
+                rules.Add (rule, rule);
+            }
+        }
+        public int RulesCount { get { return rules.Count; } }
+    }
+}
+
---
+++ test2/02_EBNF/EbnfRule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace test2
+{
+    public class EbnfRule
+    {
+        protected EbnfNonterminal nonterminal;
+        protected IEbnfRulePart [] p;
+        public EbnfRule (EbnfNonterminal nt, IEbnfRulePart [] parts)
+        {
+            nonterminal = nt;
+            p = parts;
+        }
+        public EbnfNonterminal Nonterminal { get { return nonterminal; } }
+        public IEnumerable<IEbnfRulePart> Parts { get { return p; } }
+    }
+}
+
